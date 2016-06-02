304a5fb3f917389ed920d1d1f4f9ef3edf2be3c3
zhongshanhao <572108581@qq.com>
2016-06-02 19:38:14 +0800

    ajax已完成，未命名

---
+++ my_web/js/ajax.js
@@ -0,0 +1,21 @@
+﻿function ajax(url, onsuccess)
+{
+    var xmlhttp = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP'); 
+    xmlhttp.open("POST", url, true);    
+    xmlhttp.onreadystatechange = function ()
+    {
+        if (xmlhttp.readyState == 4) 
+        {
+            if (xmlhttp.status == 200) 
+            {
+                onsuccess(xmlhttp.responseText);
+            }
+            else
+            {
+                alert("AJAX服务器返回错误！");
+            }
+        }
+    }
+  
+    xmlhttp.send(); 
+}
--- my_web/login.ashx.cs
+++ my_web/login.ashx.cs
@@ -12,35 +12,33 @@ namespace my_web
     /// </summary>
     public class login : IHttpHandler
     {
-        public void ProcessRequest(HttpContext context)
+        public void  ProcessRequest(HttpContext context)
         {
             context.Response.ContentType = "text/html";
             string str = context.Request["use"];
-            string str1 = context.Request["password3"];
+            string str1 = context.Request["password3"];          
             DataTable ds = Sqlhelper1.ExecuteDataTable("select * from student where id=@id and  keys=@keys", new SqlParameter("@id", str), new SqlParameter("@keys", str1));
             if (ds.Rows.Count == 1)
             {
-                context.Response.Write("登陆成功\n");
+                context.Response.Write("ok");
                 foreach (DataRow row in ds.Rows)
                 {
                     int id = (int)row["id"];
                     string keys = (string)row["keys"];
                     string name = (string)row["name"];
                     string xingbie = row["xingbie"].ToString();
-                    int age = (int)row["age"];
-
+                    int age = (int)row["age"];                
                     context.Response.SetCookie(new HttpCookie("id", id.ToString()));
                     //context.Response.SetCookie(new HttpCookie("name", name.ToString()));
                     //context.Response.SetCookie(new HttpCookie("xingbie", xingbie.ToString()));
                     //context.Response.SetCookie(new HttpCookie("age", age.ToString()));
-
-                    context.Response.Redirect("my_info.aspx");
+                    //context.Response.Redirect("my_info.aspx");
                     //context.Response.Write("姓名:" + name + "性别:" + xingbie + "年龄:" + age + "\n账号:" + id + "\n密码:" + keys);
                 }
             }
             else
-                context.Response.Write("<script>alert('用户名或密码错误');location.href = 'login.html';</script>");
-           
+                context.Response.Write("error");
+            
             
         }
 
